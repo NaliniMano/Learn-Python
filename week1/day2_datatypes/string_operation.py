fb85d3c6a2efa6c54447fd48bccecdec9600bf38
NaliniMano <nalinimano@gmail.com>
2024-08-04 20:17:51 +0530

    added string operation

---
+++ week1/day2_datatypes/string_operation.py
@@ -0,0 +1,17 @@
+get_input=input("Enter a String")
+print(get_input)
+str_join=get_input+"Python"
+print("Concat",str_join)
+
+str_reverse= get_input[::-1]
+print(" Reverse Order =",str_reverse)
+
+str_upper =input("Enter  string")
+print("Upper CAse ",str_upper.upper())
+
+str_substring = input("Enter String")
+print("Change the first char to Caps =", str_substring.title())
+
+print("Replace =", str_substring.replace("s","p"))
+
+
