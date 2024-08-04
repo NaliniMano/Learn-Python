4b0b507e6b527a349ea966d0aa055dc070591abe
NaliniMano <nalinimano@gmail.com>
2024-08-04 19:30:13 +0530

    added basic arth operation

---
+++ week1/day2_datatypes/basic_arth_operation.py
@@ -0,0 +1,22 @@
+get_int =input("Enter number")
+a =int(get_int)
+print("Add=", (a+10))
+print("sub=", (a -10))
+print("Mul=", (a*10))
+print("Div=",int(a/10))
+print("Mod=",a % 110)
+print("Percentage=",(a/20)*100)
+
+get_float = input("Enter float number")
+nums=float(get_float)
+print("Float Add =", (nums+5.5))
+print("Float Sub =", (nums-5.5))
+print("Float Mul =", (nums*5.5))
+print("Float Div =", (nums/5.5))
+print("Float Mod =", (nums%5.5))
+
+
+
+
+
+
