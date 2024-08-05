206dddfbd801da4f22734399636cd307df669695
NaliniMano <nalinimano@gmail.com>
2024-08-05 22:36:21 +0530

    added datatype supported

---
+++ week1/day2_datatypes/python_datatypes.py
@@ -0,0 +1,24 @@
+x=20
+print("Data type ",x,(type(x)))
+y=2.0
+print("Data type ",y,(type(y)))
+m=1j
+print("Data type ",m,(type(m)))
+
+n=["apple","orange","mango"]
+print("Data type ",n ,(type(n)))
+
+p=("ford","bmw","kia")
+print("Data type ", p ,(type(p)))
+
+e={"key":"value","name":"aaa","name":"bbb","age":30}
+print("Data type ", e ,(type(e)))
+
+s={"aa","bb","cc","aa","bb","dd"}
+print("Data type ", s ,(type(s)))
+
+t=True
+print("Data type ", t ,(type(t)))
+
+
+
