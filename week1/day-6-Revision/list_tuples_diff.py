b8080dd94da465da3c48648986ccdf2f35023da8
NaliniMano <nalinimano@gmail.com>
2024-08-09 20:49:59 +0530

    added program to diff list & tupples

---
+++ week1/day-6-Revision/list_tuples_diff.py
@@ -0,0 +1,8 @@
+mylist=["aa","bb","cc"]
+mylist.append("dd")
+mylist[0]="aaa"
+print(mylist)
+
+mytuples=(1,2,3)
+#mytuples[0]=0  #if we include this line will get TypeError: 'tuple' object does not support item assignment
+print(mytuples)
