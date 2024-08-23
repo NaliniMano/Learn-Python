25bf1d3d9b3ee445960256c1d4d3ab539bd0fa38
NaliniMano <nalinimano@gmail.com>
2024-08-23 22:31:40 +0530

    added numpy intro

--- week3/nummy/array_slicing.py
+++ week3/nummy/array_slicing.py
@@ -3,3 +3,12 @@
 import numpy as np
 arr=np.array([1,2,3,4,5,6,7,8])
 print(arr[1:5])
+
+#Slice elements from index 4 to the end of the array:
+print(arr[4:])
+
+#Slice elements from the beginning to index 4 (not included):
+print(arr[:4])
+
+#slice elements from reverse order
+print(arr[-1])
