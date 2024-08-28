802a7e261f45e02acd088bd9778d10e0fc5073c9
NaliniMano <nalinimano@gmail.com>
2024-08-28 22:15:32 +0530

    added dictionary program

---
+++ week4/dictionary_iteration.py
@@ -0,0 +1,18 @@
+thisdist={
+    "name":"Ram",
+    "age":45,
+    "bloodgrop":"B+ve",
+    "name":"jamy"
+}
+
+for x in thisdist:
+    print(x) # default  return keys of dict
+
+for y in thisdist.values():
+    print("Values of Dict ",y) # print only values
+
+for x in thisdist.keys():
+    print("Keys of Dict ,",x)
+
+for x,y in thisdist.items():
+    print("keys=",x,"Values=",y)
