444f933eb672408294563e4ca7434e83d40b1619
NaliniMano <nalinimano@gmail.com>
2024-09-03 22:28:13 +0530

    added python revision

--- Assessment/python_beginner/SecondLargestNumber_list.py
+++ Assessment/python_beginner/SecondLargestNumber_list.py
@@ -1,3 +1,12 @@
 list1=[3,5,2,1,9,10,-2,78]
 list1.sort()
 print(list1[-2])
+
+# second approach
+# find first max element & remove it from list1 and then again find max element from list
+
+max_element=max(list1)
+while max_element in list1:
+    list1.remove(max_element)
+
+print("Second max",max(list1))
