9d22543b914c2d04b7d1ec69d88044601cafb97d
NaliniMano <nalinimano@gmail.com>
2024-09-07 23:06:38 +0530

    added python revision

--- Assessment/python_beginner/file_handle.py
+++ Assessment/python_beginner/file_handle.py
@@ -9,3 +9,8 @@ mynewfile=open("writemodefile.txt","w")
 mynewfile.write("Happy Day")
 mynewfile.close()
 file_read("writemodefile.txt")
+
+fileappend=open("myfile.txt","a")
+fileappend.write("Added new lines \n")
+fileappend.close()
+file_read("myfile.txt")
