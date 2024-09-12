e993969d2e4851dae83e624163b225c90d3cc328
NaliniMano <nalinimano@gmail.com>
2024-09-12 22:24:46 +0530

    added  program using pytest fixture

--- pytest_demo/simple_pytest/test_fixure.py
+++ pytest_demo/simple_pytest/test_fixure.py
@@ -8,6 +8,19 @@ def setup():
 def test_add(setup):
     x=10
     y=20
-    assert x<y
+    assert x < y
+
+# tmpdir : Provides a temporary directory for writing and reading files during the test.
+# The directory is automatically deleted after the test.
+def test_write_file(tmpdir):
+    file = tmpdir.join("sample.txt")
+    file.write("Hello, pytest!")
+    assert file.read() == "Hello, pytest!"
+
+
+def test_write_file1(tmpdir):
+    file=tmpdir.join("sample.txt")
+    file.write("Happy coding with python")
+    assert file.read() == "Happy coding with python"
 
 
